ca09d0131301b76eced9c9cab0adafc9d8891368
phuxiteo3 <74146862+phuxiteo3@users.noreply.github.com>
2021-09-30 20:31:49 +0700

    Add files via upload

---
+++ src/demo2/Demo211.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demo2;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Dell
+ */
+public class Demo211 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a, b;
+        a = 3;
+        b = 2;
+        int result1 = Sub(a, b, c);
+        System.out.println(result1);
+    }
+
+    public static int Sum(int a, int b) {
+        return a + b;
+    }
+
+    public static int Sub(int a, int b, String a) {
+        return a - b;
+    }
+
+}
