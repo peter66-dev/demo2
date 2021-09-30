c5cb23b457489780032d2ec7ec87678400a8c734
phuxiteo3 <74146862+phuxiteo3@users.noreply.github.com>
2021-09-30 21:58:49 +0700

    Add files via upload

---
+++ src/demo2/Demo2111.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demo;
+
+/**
+ *
+ * @author Dell
+ */
+public class Demo2111 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a, b;
+        a = 3;
+        b = 5;
+        int result1 = a + b;
+        if (result1 > 9) {
+            System.out.println(result1);
+        } else {
+            System.out.println(result1 + 10);
+        }
+    }
+
+}
